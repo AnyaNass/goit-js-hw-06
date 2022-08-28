791d6ed3bdc03ace56835b5444470b659662e43d
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-08-28 22:43:20 +0300

    all except width and height

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -8,25 +8,45 @@ const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const boxes = document.querySelector('#boxes')
 
-
-number.addEventListener('input', onInput);
-
-function onInput() {
-	const numberValue = Number(number.value);
-	console.log(numberValue);
-
-	createBtn.addEventListener('click', onClick);
+number.addEventListener("input", onInputNumber);
 
 
-
-	function onClick() {
-		console.log('click');
-		const marcup = `<div style="height: 30px; width: 30px;">Box</div>`;
-		boxes.insertAdjacentHTML("afterbegin", marcup);
-	}
+function onInputNumber(event) {
+	// console.log(Number(event.target.value));
+	createBoxes(Number(event.target.value))
 }
 
-// получили число
-// сделали массив из него с фором наверное 
-// релюсом добавили лиы
+createBtn.addEventListener('click', createBoxes)
+
+function createBoxes(amount) {
+	createBtn.addEventListener('click', () => {
+		for (let i = 1; i <= amount; i += 1) {
+			const murkap = `<div class="box" style="background-color: ${getRandomHexColor()}; width: ${add10(30)}px; height: 30px;"></div>`;
+			boxes.insertAdjacentHTML('afterbegin', murkap)
+			console.log(murkap);
+		}
+		function add10(num) {
+			for (let i = 0; i <= 100; i += 10) {
+				let sum = num;
+				sum += i;
+				return sum;
+			}
+		}
+	})
+}
+
+
+// for (let i = 0; i <= 100; i += 10) {
+// 	let sum = 30;
+// 	sum += i;
+// 	console.log(sum);
+// }
+
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+	boxes.innerHTML = '';
+}
+
+
 
